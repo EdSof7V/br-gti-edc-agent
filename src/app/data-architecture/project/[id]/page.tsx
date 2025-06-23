488c03c1a4a7da35d96dfae1ca414874bec2a392
Edgar Almoguera <edgar.almoguera@analitika.cloud>
2025-06-23 14:48:50 -0500

    update files

--- src/app/data-architecture/project/[id]/page.tsx
+++ src/app/data-architecture/project/[id]/page.tsx
@@ -1,5 +1,6 @@
 import ProjectDetailsPage from "@/features/data-architecture/components/ProjectDetailsPage";
 
-export default function ProjectPage({ params }: { params: { id: string } }) {
-    return <ProjectDetailsPage projectId={params.id} />;
+export default async function ProjectPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    return <ProjectDetailsPage projectId={id} />;
 } 
