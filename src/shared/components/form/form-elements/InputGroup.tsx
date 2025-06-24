9dbd50bacd11c508ea5a2cd1dc0b0331434c537e
Edgar Almoguera <edgar.almoguera@analitika.cloud>
2025-06-24 01:15:08 -0500

    ypdate icons

--- src/shared/components/form/form-elements/InputGroup.tsx
+++ src/shared/components/form/form-elements/InputGroup.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ComponentCard from "../../common/ComponentCard";
 import Label from "../Label";
 import Input from "../input/InputField";
-import { EnvelopeIcon } from "../../../../icons";
+// import { EnvelopeIcon } from "../../../../icons";
 import PhoneInput from "../group-input/PhoneInput";
 
 export default function InputGroup() {
@@ -28,7 +28,10 @@ export default function InputGroup() {
               className="pl-[62px]"
             />
             <span className="absolute left-0 top-1/2 -translate-y-1/2 border-r border-gray-200 px-3.5 py-3 text-gray-500 dark:border-gray-800 dark:text-gray-400">
-              <EnvelopeIcon />
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2.5 5.83333C2.5 4.91286 3.24619 4.16667 4.16667 4.16667H15.8333C16.7538 4.16667 17.5 4.91286 17.5 5.83333V14.1667C17.5 15.0871 16.7538 15.8333 15.8333 15.8333H4.16667C3.24619 15.8333 2.5 15.0871 2.5 14.1667V5.83333Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                <path d="M17.5 5.83333L10 10.8333L2.5 5.83333" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
             </span>
           </div>
         </div>
